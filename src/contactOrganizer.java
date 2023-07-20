ff1c2af2cb5fbad8708d0e808c9a96322194ac84
Binuka99x <binukag1@gmail.com>
2023-07-20 22:54:33 +0530

    first commit

---
+++ src/contactOrganizer.java
@@ -0,0 +1,25 @@
+class Contacts {
+    private String id;
+    private String name;
+    private String phoneNumber;
+    private String companyName;
+    private String dob;
+    private double salary;
+
+    Contacts(String id, String name, String phoneNumber, String companyName, double salary, String dob) {
+        this.id = id;
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+        this.companyName = companyName;
+        this.salary = salary;
+        this.dob = dob;
+    }
+}
+class ContactList{
+
+}
+public class contactOrganizer {
+    public static void main(String[] args) {
+
+    }
+}
